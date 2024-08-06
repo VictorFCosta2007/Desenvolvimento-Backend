4b6dbc28b619d99604dd066e5d752a75184c8cf6
VictorFCosta2007 <victorferreiracosta@gmail.com>
2024-08-06 18:35:40 -0300

    Lista 1

---
+++ Listas/lista01/entradaPermitida/src/test/java/com/example/entradaPermitida/EntradaPermitidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entradaPermitida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntradaPermitidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Listas/lista01/projetoSafari/src/test/java/com/example/projetoSafari/ProjetoSafariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoSafari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSafariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
